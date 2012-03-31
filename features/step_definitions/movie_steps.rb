9ee50fe559126a2774687be9157b7315936d0215
saasbook <saasbook@saasbook.(none)>
2012-03-31 16:05:11 +0200

    hw 3 part 2b start

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -26,4 +26,10 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+   rating_list.split(',').each do |rating| 
+    steps %Q{
+        When I #{uncheck}check "ratings_#{rating}"
+        Then the "ratings_#{rating}" checkbox should be checked
+    }
+  end
 end
